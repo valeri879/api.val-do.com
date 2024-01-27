1e9e4054dcad840ab35785734dce86c90a45ef98
Val Do <valeri.kharitonashvili1@gmail.com>
2024-01-28 02:45:27 +0400

    converter

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 require("./start/routes.start")(app);
 require("./start/db.start")();
 
+const fs = require('fs');
+const sharp = require('sharp');
+
+fs.readdirSync('uploads').forEach(file => {
+    // console.log(file)
+    const imgRegex = /\.(jpg|png|jpeg)\b/i;
+    if (imgRegex.test(file)) {
+        sharp(`./uploads/${file}`).resize({ width: 320 }).toFile(`./uploads/small/${file}`).then(() => { });
+    }
+})
+
+
 if (!process.env.PRIVATE_KEY) {
 	process.exit(1);
 }
